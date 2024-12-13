e1510c91bae8c14816439a9f2ca4ba9307be85bd
Shahin M. Shahin <peregrineshahin@gmail.com>
2024-12-13 17:03:27 +0300

    Fix bug that existed in old SF
    bench: 1707420

--- src/search.c
+++ src/search.c
@@ -238,8 +238,10 @@ void thread_search(Position* pos) {
     }
     (ss - 1)->endMoves = pos->moveList;
 
-    for (int i = -7; i < 0; i++)
+    for (int i = -7; i < 0; i++) {
         ss[i].history = &(*pos->counterMoveHistory)[0][0];  // Use as sentinel
+        ss[i].staticEval = VALUE_NONE;
+    }
 
     for (int i = 0; i <= MAX_PLY; i++)
         ss[i].ply = i;
