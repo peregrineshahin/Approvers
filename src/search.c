72e3bb8b84e1c1860a87cd087ac6c9ad528a13d2
Shahin M. Shahin <peregrineshahin@gmail.com>
2024-12-15 22:16:45 +0300

    Revert "do shallower only"
    This reverts commit 848b856a2cc2cf40bc7966121836531cbb595453.

--- src/search.c
+++ src/search.c
@@ -956,9 +956,10 @@ moves_loop:  // When in check search starts from here.
 
                 // Adjust full-depth search based on LMR results - if the result was
                 // good enough search deeper, if it was bad enough search shallower.
-                const bool doShallowerSearch = value < bestValue + 16;
+                const bool doDeeperSearch    = value > (bestValue + 42 + 2 * newDepth);
+                const bool doShallowerSearch = value < bestValue + 10;
 
-                newDepth += - doShallowerSearch;
+                newDepth += doDeeperSearch - doShallowerSearch;
 
                 if (newDepth > d)
                     value = -search(pos, ss + 1, -(alpha + 1), -alpha, newDepth, !cutNode, false);
