04adc58dd59690b3ba3cf6a7024be26116a27d11
Shahin M. Shahin <peregrineshahin@gmail.com>
2024-12-15 22:18:14 +0300

    Update search.c
    bench: 1888361

--- src/search.c
+++ src/search.c
@@ -956,10 +956,9 @@ moves_loop:  // When in check search starts from here.
 
                 // Adjust full-depth search based on LMR results - if the result was
                 // good enough search deeper, if it was bad enough search shallower.
-                const bool doDeeperSearch    = value > (bestValue + 42 + 2 * newDepth);
-                const bool doShallowerSearch = value < bestValue + 10;
+                const bool doDeeperSearch = value > (bestValue + 42 + 2 * newDepth);
 
-                newDepth += doDeeperSearch - doShallowerSearch;
+                newDepth += doDeeperSearch;
 
                 if (newDepth > d)
                     value = -search(pos, ss + 1, -(alpha + 1), -alpha, newDepth, !cutNode, false);
