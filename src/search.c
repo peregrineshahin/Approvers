bda7f1eff723ab57114237f94270879736e4090b
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2024-12-17 10:05:04 +0300

    Simplify LMR conditions (#8)
    Bench: 690429

--- src/search.c
+++ src/search.c
@@ -955,9 +955,8 @@ moves_loop:  // When in check search starts from here.
 
         // Step 16. Reduced depth search (LMR). If the move fails high it will be
         // re-searched at full depth.
-        if (depth >= 3 && moveCount > 1 + 2 * rootNode + 2 * (PvNode && abs(bestValue) < 2)
-            && (!captureOrPromotion || moveCountPruning
-                || ss->staticEval + *PieceValue[EG][captured_piece()] <= alpha || cutNode))
+        if (depth >= 3 && moveCount > 1 + 2 * rootNode
+            && (!captureOrPromotion || cutNode || (!PvNode && !formerPv)))
         {
             Depth r = reduction(improving, depth, moveCount);
 
