d34d26477accf1bf4042d50255823a459b0f18a9
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2024-12-18 18:50:33 +0300

    Create main.py
    Bench: 668257

---
+++ scripts/main.py
@@ -0,0 +1,15 @@
+import os
+import subprocess
+
+p = "/kaggle_simulations/agent/cfish"
+os.system("chmod +x " + p)
+e = subprocess.Popen([p], stdin=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True)
+
+def w(v):
+    e.stdin.write(v)
+    e.stdin.flush()
+
+def main(v):
+    t = v.remainingOverageTime * 1000
+    w(f"stop\nposition fen {v.board}\ngo wtime {str(t)} btime {str(t)}\n")
+    return e.stdout.readline().strip().split()[1]
