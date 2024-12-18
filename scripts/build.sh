d83a295993b8b70cdc5504e67086ed18e3354670
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2024-12-18 18:52:57 +0300

    Create build.sh
    Bench: 668257

---
+++ scripts/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p ../build
+
+cd ../src || exit
+make -j build CFLAGS="-DKAGGLE -fcommon -Os -s -ffunction-sections -fdata-sections -flto -mavx2" LDFLAGS="-Wl,--gc-sections" ARCH=x86-64-avx2
+strip cfish
+
+cd ../build || exit
+
+tar --transform 's/.*\///' -cf submission.tar ../src/cfish ../scripts/main.py
+zopfli submission.tar
+ls -l
