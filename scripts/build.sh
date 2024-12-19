57b9ce721a363ed2e55ee7c076a7a68194369fa5
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2024-12-19 06:46:46 +0000

    Update build.sh
    Bench: 506022

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,7 @@
 mkdir -p ../build
 
 cd ../src || exit
+make clean
 make -j build CFLAGS="-DKAGGLE -fcommon -Os -s -ffunction-sections -fdata-sections -flto -mavx2" LDFLAGS="-Wl,--gc-sections" ARCH=x86-64-avx2
 strip cfish
 
