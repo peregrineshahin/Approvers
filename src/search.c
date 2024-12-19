59229f5c53a576021ac05e5dda1f8d1bc868fd58
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2024-12-19 22:00:54 +0300

    Bench: 493963

--- src/search.c
+++ src/search.c
@@ -943,7 +943,7 @@ moves_loop:  // When in check search starts from here.
 
                 // Increase reduction for cut nodes
                 if (cutNode)
-                    r += 2;
+                    r += 1 + !(ss->ttPv && !PvNode);
 
                 // Decrease reduction for moves that escape a capture. Filter out
                 // castling moves, because they are coded as "king captures rook" and
