52d7de7e31fc670237af84b67b42affe734f8250
Shahin M. Shahin <peregrineshahin@gmail.com>
2024-12-20 15:33:00 +0300

    don't skip full windows
    bench: 465657

--- src/search.c
+++ src/search.c
@@ -989,7 +989,7 @@ moves_loop:  // When in check search starts from here.
         // For PV nodes only, do a full PV search on the first move or after a fail
         // high (in the latter case search only if value < beta), otherwise let the
         // parent node fail low with value <= alpha and try another move.
-        if (PvNode && (moveCount == 1 || (value > alpha && (rootNode || value < beta))))
+        if (PvNode && (moveCount == 1 || value > alpha))
         {
             (ss + 1)->pv    = pv;
             (ss + 1)->pv[0] = 0;
