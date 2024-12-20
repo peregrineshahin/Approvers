e0efad530c5db5ca8e20ad0650ad6b1476430582
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2024-12-20 21:12:29 +0300

    Bench: 2090967

--- src/search.c
+++ src/search.c
@@ -764,11 +764,11 @@ moves_loop:  // When in check search starts from here.
 
             if (!captureOrPromotion && !givesCheck)
             {
-                // Countermoves based pruning
+                // Continuation history based pruning
                 if (lmrDepth < cbp_v1 / 100
                                  + ((ss - 1)->statScore > cbp_v2 / 100 || (ss - 1)->moveCount == 1)
-                    && (*cmh)[movedPiece][to_sq(move)] < CounterMovePruneThreshold
-                    && (*fmh)[movedPiece][to_sq(move)] < CounterMovePruneThreshold)
+                    && (*cmh)[movedPiece][to_sq(move)] < (depth == 1 ? 0 : -stat_bonus(depth - 1))
+                    && (*fmh)[movedPiece][to_sq(move)] < (depth == 1 ? 0 : -stat_bonus(depth - 1)))
                     continue;
 
                 // Futility pruning: parent node
