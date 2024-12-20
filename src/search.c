dca18a0d0218b66e46ffcc68bee0a156fb4f5b8d
Shahin M. Shahin <peregrineshahin@gmail.com>
2024-12-20 07:45:57 +0300

    rootdepth
    bench: 471609

--- src/search.c
+++ src/search.c
@@ -339,7 +339,7 @@ void thread_search(Position* pos) {
 
     // Iterative deepening loop until requested to stop or the target depth
     // is reached.
-    while ((pos->rootDepth += 2) < MAX_PLY && !Thread.stop
+    while ((pos->rootDepth += (pos->rootDepth / 2) % 2 == 0 ? 2 : 3) < MAX_PLY && !Thread.stop
            && !(Limits.depth && pos->rootDepth > Limits.depth))
     {
         // Age out PV variability metric
