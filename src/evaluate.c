2fd7db1b5e5e9e25ef8e1d180cd6c9aa16c9fa88
Shahin M. Shahin <peregrineshahin@gmail.com>
2024-12-22 13:09:56 +0300

    bench: 2037514

--- src/evaluate.c
+++ src/evaluate.c
@@ -30,7 +30,10 @@ Value evaluate(Position* pos) {
 
     v = eval_scale * v / 100;
 
-    v = v * (26500 + non_pawn_material()) / 32768;
+    v = v
+      * (20100 + non_pawn_material()
+         + PawnValue * (piece_count(WHITE, PAWN) + piece_count(BLACK, PAWN)))
+      / 32768;
 
     // Damp down the evaluation linearly when shuffling
     v = v * (100 - rule50_count()) / 100;
