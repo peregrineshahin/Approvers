aebd66a7c95e51c353a8cb5c360ee241470fa190
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2025-01-03 04:33:29 +0300

    Bench: 2090628

--- src/types.h
+++ src/types.h
@@ -270,7 +270,7 @@ struct PVariation {
 
 enum {
     CORRECTION_HISTORY_ENTRY_NB = 16384,
-    CORRECTION_HISTORY_MAX      = 8192,
+    CORRECTION_HISTORY_MAX      = 11000,
 };
 
 typedef int16_t        PieceToHistory[15][64];
