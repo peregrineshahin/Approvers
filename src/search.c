42d91d8ec1cdf13e56616792a733263cef0433fb
Shahin M. Shahin <peregrineshahin@gmail.com>
2025-01-05 18:49:33 +0300

    bench: 1626730

--- src/search.c
+++ src/search.c
@@ -725,7 +725,7 @@ moves_loop:  // When in check search starts from here.
                             + (*contHist0)[movedPiece][to_sq(move)]
                             + (*contHist1)[movedPiece][to_sq(move)];
 
-                lmrDepth += history / 3609;
+                lmrDepth += history / 4609;
 
                 // Futility pruning: parent node
                 if (lmrDepth < fpp_v1 && !inCheck
