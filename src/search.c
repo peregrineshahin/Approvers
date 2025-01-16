78ee5ae2d2c46d713ec448ba127866530317e3bf
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2025-01-16 09:22:12 +0300

    Introduce minor major correction history (#105)
    LLR   | 2.91 (-2.25, 2.89) [0.00, 3.00]
    Games | N: 33638 W: 8407 L: 8145 D: 17086
    Penta | [200, 4036, 8077, 4314, 192]
    
    Bench: 2261369

--- src/search.c
+++ src/search.c
@@ -113,6 +113,8 @@ PARAM(ch_v5, 137, 12.0)
 PARAM(ch_v6, 131, 12.0)
 PARAM(ch_v7, 119, 12.0)
 PARAM(ch_v8, 121, 12.0)
+PARAM(ch_v9, 128, 12.0)
+PARAM(ch_v10, 96, 12.0)
 PARAM(tempo, 45, 4.8)
 PARAM(mp_v3, 2623, 180.0)
 PARAM(mp_v4, 188, 12.0)
@@ -1190,7 +1192,8 @@ static Value value_from_tt(Value v, int ply, int r50c) {
 
 
 static void update_correction_histories(const Position* pos, Depth depth, int32_t diff) {
-    Key keys[] = {material_key(), pawn_key(), prev_move_key(), w_nonpawn_key(), b_nonpawn_key()};
+    Key keys[] = {material_key(),  pawn_key(),  prev_move_key(), w_nonpawn_key(),
+                  b_nonpawn_key(), minor_key(), major_key()};
 
 #pragma clang loop unroll(disable)
     for (size_t i = 0; i < CORRECTION_HISTORY_NB; i++)
@@ -1206,8 +1209,9 @@ static void update_correction_histories(const Position* pos, Depth depth, int32_
 }
 
 Value to_corrected(Position* pos, Value unadjustedStaticEval) {
-    Key keys[]    = {material_key(), pawn_key(), prev_move_key(), w_nonpawn_key(), b_nonpawn_key()};
-    int weights[] = {ch_v4, ch_v5, ch_v6, ch_v7, ch_v8};
+    Key keys[]    = {material_key(),  pawn_key(),  prev_move_key(), w_nonpawn_key(),
+                     b_nonpawn_key(), minor_key(), major_key()};
+    int weights[] = {ch_v4, ch_v5, ch_v6, ch_v7, ch_v8, ch_v9, ch_v10};
 
     int32_t correction = 0;
     for (size_t i = 0; i < CORRECTION_HISTORY_NB; i++)
