d4222dc6bb4c77504fc452d3ee3e67776827ad74
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2025-01-23 17:47:42 +0300

    Bench: 2776938

--- scripts/main.py
+++ scripts/main.py
@@ -9,5 +9,5 @@ def w(v):
 
 def main(v):
     t = v.remainingOverageTime * 1000
-    w(f"stop\nposition fen {v.board}\ngo wtime {str(t)} btime {str(t)}\n")
+    w(f"stop\nkpos fen {v.board} moves {v.lastMove}\ngo wtime {str(t)} btime {str(t)}\n")
     return e.stdout.readline().strip().split()[1]
