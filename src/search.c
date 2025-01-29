719850ddf789bd90f1e24357400c50c241b51263
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2025-01-29 05:40:59 +0300

    Bench: 2248368

--- src/search.c
+++ src/search.c
@@ -874,7 +874,7 @@ moves_loop:  // When in check search starts from here.
             r -= r_v14;
 
         // Decrease/increase reduction for moves with a good/bad history.
-        r -= ss->statScore * r_v13 / 16384;
+        r -= ss->statScore * (r_v13 - 384 * (depth >= 10)) / 16384;
 
         // Step 14. Late move reductions (LMR)
         if (depth >= 2 && moveCount > 1 && (!capture || !ss->ttPv))
