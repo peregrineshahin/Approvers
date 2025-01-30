9461f8f7e60f23a3539158df664ed583ce6f0a86
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2025-01-30 11:04:11 +0300

    Bench: 1639504

--- src/nnue.c
+++ src/nnue.c
@@ -74,35 +74,19 @@ static Value output_transform(const Accumulator* acc, const Position* pos) {
     return (output / QA + l1_biases[bucket]) * SCALE / (QA * QB);
 }
 
-static void refresh_accumulator(Accumulator* acc, const Position* pos, Color side) {
-    const __m256i* biases                 = (__m256i*) in_biases;
-    __m256i        registers[L1SIZE / 16] = {
-      biases[0],
-      biases[1],
-      biases[2],
-      biases[3],
-    };
+static void refresh_accumulators(Accumulator* acc, const Position* pos) {
+    memcpy(acc->values[WHITE], in_biases, sizeof(in_biases));
+    memcpy(acc->values[BLACK], in_biases, sizeof(in_biases));
 
-    const Square ksq = square_of(side, KING);
+    const Square wksq = square_of(WHITE, KING);
+    const Square bksq = square_of(BLACK, KING);
+
     for (Bitboard pieces = pieces(); pieces;)
     {
         const Square sq = pop_lsb(&pieces);
         const Piece  pc = piece_on(sq);
-
-        const int      index   = make_index(type_of_p(pc), color_of(pc), sq, ksq, side);
-        const __m256i* weights = (__m256i*) &in_weights[index * L1SIZE];
-
-        registers[0] = _mm256_add_epi16(registers[0], weights[0]);
-        registers[1] = _mm256_add_epi16(registers[1], weights[1]);
-        registers[2] = _mm256_add_epi16(registers[2], weights[2]);
-        registers[3] = _mm256_add_epi16(registers[3], weights[3]);
+        nnue_add_piece(acc, pc, sq, wksq, bksq);
     }
-
-    __m256i* values = (__m256i*) &acc->values[side];
-    values[0]       = registers[0];
-    values[1]       = registers[1];
-    values[2]       = registers[2];
-    values[3]       = registers[3];
 }
 
 void nnue_add_piece(Accumulator* acc, Piece pc, Square sq, Square wksq, Square bksq) {
@@ -144,8 +128,7 @@ Value nnue_evaluate(Position* pos) {
 
     if (acc->needs_refresh)
     {
-        refresh_accumulator(acc, pos, WHITE);
-        refresh_accumulator(acc, pos, BLACK);
+        refresh_accumulators(acc, pos);
         acc->needs_refresh = false;
     }
 
