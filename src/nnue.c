5f99d1ab73a1db1b43117ab382fcfa5b689ab8b9
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2025-01-30 03:21:21 +0300

    Transpose
    Bench: 2171075

--- src/nnue.c
+++ src/nnue.c
@@ -30,9 +30,9 @@ SMALL void nnue_init() {
     for (int i = 0; i < L1SIZE; i++)
         in_biases[i] = *(data++);
 
-    for (int i = 0; i < L1SIZE * 2; i++)
-        for (int j = 0; j < BUCKETS; j++)
-            l1_weights[j][i] = *(data++);
+    for (int i = 0; i < BUCKETS; i++)
+        for (int j = 0; j < L1SIZE * 2; j++)
+            l1_weights[i][j] = *(data++);
 
     int16_t* data16 = (int16_t*) data;
 
