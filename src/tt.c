cd9a511e167d6184ec04a6deaabca6a562e5b3fa
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2025-01-31 19:09:39 +0300

    Bench: 1640366

--- src/tt.c
+++ src/tt.c
@@ -99,8 +99,8 @@ TTEntry* tt_probe(Key key, bool* found) {
         // nature we add 263 (256 is the modulus plus 7 to keep the unrelated
         // lowest three bits from affecting the result) to calculate the entry
         // age correctly even after generation8 overflows into the next cycle.
-        if (replace->depth8 - ((263 + TT.generation8 - replace->genBound8) & 0xF8)
-            > tte[i].depth8 - ((263 + TT.generation8 - tte[i].genBound8) & 0xF8))
+        if (replace->depth8 - 4 * ((263 + TT.generation8 - replace->genBound8) & 0xF8)
+            > tte[i].depth8 - 4 * ((263 + TT.generation8 - tte[i].genBound8) & 0xF8))
             replace = &tte[i];
 
     *found = false;
