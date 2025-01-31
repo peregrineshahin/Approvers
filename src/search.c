ec99ad51db8785f94239dd7255d11ccf9ed9c545
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2025-01-31 04:13:44 +0300

    Bench: 1835387

--- src/search.c
+++ src/search.c
@@ -74,8 +74,8 @@ PARAM(nmp_v8, 95)
 PARAM(nmp_v9, 208)
 PARAM(d_v1, 18)
 PARAM(cbp_v1, 4)
-PARAM(cbp_v2, 500)
-PARAM(cbp_v3, 500)
+PARAM(cbp_v2, 750)
+PARAM(cbp_v3, 750)
 PARAM(fpp_v1, 7)
 PARAM(fpp_v2, 263)
 PARAM(fpp_v3, 160)
@@ -753,6 +753,7 @@ moves_loop:  // When in check search starts from here.
                 // Continuation history based pruning
                 if (lmrDepth < cbp_v1
                     && (*contHist0)[movedType][to_sq(move)] + (*contHist1)[movedType][to_sq(move)]
+                           + (*contHist2)[movedType][to_sq(move)]
                          < -cbp_v2 * depth + cbp_v3)
                     continue;
 
