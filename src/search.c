2803d8226137807918db7b88068b25f8b467d1a5
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2025-02-03 10:20:16 +0300

    Bench: 2233669

--- src/search.c
+++ src/search.c
@@ -788,7 +788,7 @@ moves_loop:  // When in check search starts from here.
             && tte_bound(tte) & BOUND_LOWER && tte_depth(tte) >= depth - 3
             && abs(ttValue) < VALUE_MATE_IN_MAX_PLY)
         {
-            Value singularBeta  = ttValue - se_v2 * depth / 1024;
+            Value singularBeta  = ttValue - (1 + (ss->ttPv && !PvNode)) * depth;
             Depth singularDepth = newDepth / 2;
             ss->excludedMove    = move;
             Move k1 = ss->mpKillers[0], k2 = ss->mpKillers[1];
