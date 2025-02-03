ee1223c67d7624eb1887d4e158f80e99021423ca
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2025-02-03 04:36:04 +0300

    Bench: 2555913

--- src/nnue.c
+++ src/nnue.c
@@ -8,7 +8,7 @@
 #include "bitboard.h"
 #include "position.h"
 
-INCBIN(Network, "../default.nnue");
+INCBIN(Network, "../expt-35.nnue");
 
 alignas(64) int16_t in_weights[INSIZE * L1SIZE];
 alignas(64) int16_t in_biases[L1SIZE];
