22c16b774ae7904ce33aa1de1b93ca99986e6696
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2025-02-07 09:50:20 +0300

    Bench: 1850257

--- src/evaluate.c
+++ src/evaluate.c
@@ -22,24 +22,22 @@
 #include "nnue.h"
 #include "position.h"
 
-int eval_scale = 93;
-int mat_scale  = 25855;
-int mat_n      = 643;
-int mat_b      = 750;
-int mat_r      = 1461;
-int mat_q      = 2321;
+int mat_scale = 21000;
+int mat_p     = 200;
+int mat_n     = 643;
+int mat_b     = 750;
+int mat_r     = 1461;
+int mat_q     = 2321;
 
 Value evaluate(Position* pos) {
     Value v = nnue_evaluate(pos);
 
-    v = eval_scale * v / 100;
-
-    int non_pawn_material = mat_n * popcount(pieces_p(KNIGHT)) + mat_b * popcount(pieces_p(BISHOP))
-                          + mat_r * popcount(pieces_p(ROOK)) + mat_q * popcount(pieces_p(QUEEN));
+    int non_pawn_material = mat_p * popcount(pieces_p(PAWN)) + mat_n * popcount(pieces_p(KNIGHT))
+                          + mat_b * popcount(pieces_p(BISHOP)) + mat_r * popcount(pieces_p(ROOK))
+                          + mat_q * popcount(pieces_p(QUEEN));
 
     v = v * (mat_scale + non_pawn_material) / 32768;
 
-    // Damp down the evaluation linearly when shuffling
     v = v * (100 - rule50_count()) / 100;
 
     return clamp(v, VALUE_MATED_IN_MAX_PLY + 1, VALUE_MATE_IN_MAX_PLY - 1);
