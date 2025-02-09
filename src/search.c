4cb3a0d0f6f250e79f5a583fea89ea2407a5aea7
Shahin M. Shahin <peregrineshahin@gmail.com>
2025-02-09 14:39:51 +0300

    bench: 2128853

--- src/search.c
+++ src/search.c
@@ -935,7 +935,7 @@ moves_loop:  // When in check search starts from here.
         if (PvNode && (moveCount == 1 || value > alpha))
         {
             // Extend move from transposition table if we are about to dive into qsearch.
-            if (move == ttMove && ss->ply <= pos->rootDepth * 2)
+            if (move == ttMove)
                 newDepth = max(newDepth, 1);
 
             value = -search(pos, ss + 1, -beta, -alpha, newDepth, false, true);
