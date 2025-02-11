703a940124fbab591717f0bfa67a72f61ce7b695
Shahin M. Shahin <peregrineshahin@gmail.com>
2025-02-11 07:54:53 +0300

    bench: 1686729

--- src/position.h
+++ src/position.h
@@ -208,7 +208,7 @@ PURE bool has_game_cycle(const Position* pos, int ply);
 #define w_nonpawn_key() (pos->st->nonPawnKey[WHITE])
 #define b_nonpawn_key() (pos->st->nonPawnKey[BLACK])
 #define major_key() (pos->st->majorKey)
-#define minor_key() (pos->st->minorKey)
+#define minor_key() (pos->st->minorKey ^ pos->st->majorKey)
 #define prev_move_key() (from_to((pos->st - 1)->currentMove))
 
 // Other properties of the position
